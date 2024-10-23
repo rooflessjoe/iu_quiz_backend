3f71877c918b9123167e50e594096602e1c2c88f
Emil <s8emsajk@stud.uni-saarland.de>
2024-10-23 12:33:30 +0200

    Sicherheitsanpassungen

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,  // Render stellt diese Umgebungsvariable bereit
   ssl: {
-    rejectUnauthorized: false,  // Setze dies auf true für Produktionsumgebungen
+    rejectUnauthorized: true,  // Setze dies auf true für Produktionsumgebungen
     ca: process.env.DATABASE_SSL_CA // Optional: Füge das CA-Zertifikat hinzu, wenn erforderlich
   }
 });
