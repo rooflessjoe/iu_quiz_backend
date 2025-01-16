ce7d279fa3507c7a3c31ef7133083f61d50663b4
Emil <emil.sajkevic@iu-study.org>
2025-01-16 15:46:48 +0100

    APIs angepasst und Quiz APIs hinzugefügt

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const path = '/etc/secrets/secret_key'; // Pfad zur geheimen Datei auf dem Server
-const cors_origin = require('./components/cors_origin.json');
+//const cors_origin = require('./components/cors_origin.json');
 
 /** 
  * Liest den geheimen Schlüssel aus der geheimen Datei auf dem Server
