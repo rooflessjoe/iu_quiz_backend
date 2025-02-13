a5278210fd7a854f14596a6b26ab04026a115933
Emil <emil.sajkevic@iu-study.org>
2025-02-13 14:23:39 +0100

    Pool Test 4

--- components/auth.js
+++ components/auth.js
@@ -16,8 +16,8 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
-    async() => {
-    if (await token == null) {
+    //async() => {
+    if (token == null) {
       return res.sendStatus(401).send('Invalid Token');
     } else {
       jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
@@ -33,7 +33,7 @@ function authenticateToken(req, res, next) {
     });
     }
   }
-}
+//}
 
 //Statischer Token bis Registrierung und Login komplett implementiert wurden
 //const staticToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRlc3R1c2VyIiwiaWF0IjoxNjE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
