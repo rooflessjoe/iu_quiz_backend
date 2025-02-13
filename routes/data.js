76d0c46733bdabaab030daddf04ea018a87cabb3
Emil <emil.sajkevic@iu-study.org>
2025-02-13 12:29:36 +0100

    Test Pool 3

--- routes/data.js
+++ routes/data.js
@@ -1,7 +1,7 @@
 // Importieren benötigter Module
 const express = require('express');
 const cors = require('cors');
-const { Pool } = require('pg');
+const pool = require('../server');
 const authenticateToken = require('../components/auth.js');
 const queries = require('../components/queries.json');
 //const cors_origin = require('../components/cors_origin.json');
@@ -13,13 +13,13 @@ const router = express.Router();
 /** 
  * PostgreSQL-Verbindung
  */
-const pool = new Pool({
+/*const pool = new Pool({
   connectionString: process.env.DATABASE_URL,  // Server stellt diese Umgebungsvariable bereit
   ssl: {
     require: true,
     rejectUnauthorized: false,  // Setze dies auf true für Produktionsumgebungen -> benötigt ein Zertifikat
   }
-});
+});*/
 
 // CORS
 //router.use(cors({ origin: cors_origin.origin_local }));
--- routes/quiz.js
+++ routes/quiz.js
@@ -6,7 +6,7 @@
 
 //Benötigte Module
 const express = require('express');
-const { Pool } = require('pg');
+const pool = require('../server');
 const authenticateToken = require('../components/auth.js');
 const queries = require('../components/queries.json');
 
@@ -40,13 +40,13 @@ const router = express.Router();
  * @type {PoolConfig}
  * @memberof module:Quiz~PostgreSQL
  */
-const pool = new Pool({
+/*const pool = new Pool({
   connectionString: process.env.DATABASE_URL,  // Server stellt diese Umgebungsvariable bereit
   ssl: {
     require: true,
     rejectUnauthorized: false,  // Setze dies auf true für Produktionsumgebungen -> benötigt ein Zertifikat
   }
-});
+});*/
 
 /**
  * Route serving the list of available quizes
