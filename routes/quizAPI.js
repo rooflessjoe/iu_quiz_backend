81ca7b24c24d854ab248f35c74e676b0daffd5e1
NiklasG-127 <niklasgeldbach@gmail.com>
2025-02-26 11:06:52 +0100

    Comments

--- routes/quizAPI.js
+++ routes/quizAPI.js
@@ -152,8 +152,6 @@ module.exports = (io) => {
                 // activates room with given parameters
                 RoomsState.activateRoom(room, 0, questionCount, category, timerEnabled, timer, user.name);
 
-
-
                 console.log(RoomsState.rooms)
                 await userJoinsRoom(socket, user);
             }catch (error) {
@@ -176,8 +174,6 @@ module.exports = (io) => {
                     new Error('User not found');
                 }
                 const room = RoomsState.rooms.find(r => r.room === user.room);
-                console.log(user.name)
-                console.log('Host is', room.gameHost)
 
                 if (!room) {
                     new Error('Room not found for the user');
@@ -208,7 +204,6 @@ module.exports = (io) => {
 
                 // checks if question got loaded and sent question with timer to room
                 if (questions.length > 0) {
-                    console.log('timer:', room.timer, room.timerEnabled);
                     io.to(room.room).emit('question', {
                         currentQuestion: currentQuestionThisRound,
                         questionCount: room.questionCount,
@@ -245,7 +240,6 @@ module.exports = (io) => {
                 //if Answers found emit to room
                 if(answers.length > 0){
                     io.to(room).emit('answers', answers)
-                    console.log('Send')
                 }else{
                     io.to(room).emit('message', buildMsg(ADMIN, 'Keine Antwort gefunden'))
                 }
@@ -311,47 +305,7 @@ module.exports = (io) => {
 
                 // checks if all players have answered
                 if(UsersState.haveAllUsersAnswered(room.room)){
-
-                    // sets answered status for all players in room to false
-                    const usersInRoom = UsersState.getUsersInRoom(room.room);
-                    usersInRoom.forEach(u => UsersState.updateUserStatus(u.id, false));
-
-                    //checks if current question is under max questions for room
-                    if(room.currentQuestion < room.questionCount){
-                        // increases currentQuestion by 1, updates room
-                        const currentQuestionThisRound = room.currentQuestion + 1;
-                        const updates = {
-                            currentQuestion: currentQuestionThisRound
-                        };
-                        const updatedRoom = updateRoomAttribute(room.room, updates)
-
-                        // gets new question from Database
-                        const questions = await getQuestions(updatedRoom.category);
-
-                        // emits question to room
-                        io.to(updatedRoom.room).emit('question',{
-                            currentQuestion: currentQuestionThisRound,
-                            questionCount: room.questionCount,
-                            timerEnabled: room.timerEnabled,
-                            timerDuration: room.timer,
-                            question_id: questions[0].question_id,
-                            question: questions[0].question,
-                        })
-
-                    }else{
-                        // if max amount of question is reached ends game
-                        console.log('Game over')
-
-                        // gets score from all players in room and emits them to room
-                        const usersInRoom = getUsersInRoom(room.room)
-                        const userScores = usersInRoom.map(u => ({
-                            name: u.name,
-                            score: u.score
-                        }))
-                        const gameAnswersArray = room.playerAnswersArray;
-
-                        io.to(room.room).emit('quizOver', userScores, gameAnswersArray);
-                    }
+                    await sendNewQuestionsToFront(room);
                 }else {
                     // if not everyone has answered, ends here and waits for last person to answer
                     console.log('Noch nicht alle Nutzer haben geantwortet');
@@ -396,46 +350,9 @@ module.exports = (io) => {
                 if (!room) {
                     new Error('Room not found');
                 }
+                //checks if user is gamehost
                 if (user.name === room.gameHost){
-                    // sets answered status for all players in room to false
-                    const usersInRoom = UsersState.getUsersInRoom(room.room);
-                    usersInRoom.forEach(u => UsersState.updateUserStatus(u.id, false));
-                    //checks if current question is under max questions for room
-                    if(room.currentQuestion < room.questionCount){
-                        // increases currentQuestion by 1, updates room
-                        const currentQuestionThisRound = room.currentQuestion + 1;
-                        const updates = {
-                            currentQuestion: currentQuestionThisRound
-                        };
-                        const updatedRoom = updateRoomAttribute(room.room, updates)
-
-                        // gets new question from Database
-                        const questions = await getQuestions(updatedRoom.category);
-
-                        // emits question to room
-                        io.to(updatedRoom.room).emit('question',{
-                            currentQuestion: currentQuestionThisRound,
-                            questionCount: room.questionCount,
-                            timerEnabled: room.timerEnabled,
-                            timerDuration: room.timer,
-                            question_id: questions[0].question_id,
-                            question: questions[0].question,
-                        })
-
-                    }else{
-                        // if max amount of question is reached ends game
-                        console.log('Game over')
-
-                        // gets score from all players in room and emits them to room
-                        const usersInRoom = getUsersInRoom(room.room)
-                        const userScores = usersInRoom.map(u => ({
-                            name: u.name,
-                            score: u.score
-                        }))
-                        const gameAnswersArray = room.playerAnswersArray;
-
-                        io.to(room.room).emit('quizOver', userScores, gameAnswersArray);
-                    }
+                    await sendNewQuestionsToFront(room);
                 }
             }catch(err){
                 console.error('Fehler', err.stack)}
@@ -752,6 +669,48 @@ module.exports = (io) => {
             io.emit('roomList', {
                 rooms: getAllActiveRooms(),
             });
+        }
+    }
+
+    async function sendNewQuestionsToFront (room){
+        // sets answered status for all players in room to false
+        const usersInRoom = UsersState.getUsersInRoom(room.room);
+        usersInRoom.forEach(u => UsersState.updateUserStatus(u.id, false));
+        //checks if current question is under max questions for room
+        if(room.currentQuestion < room.questionCount){
+            // increases currentQuestion by 1, updates room
+            const currentQuestionThisRound = room.currentQuestion + 1;
+            const updates = {
+                currentQuestion: currentQuestionThisRound
+            };
+            const updatedRoom = updateRoomAttribute(room.room, updates)
+
+            // gets new question from Database
+            const questions = await getQuestions(updatedRoom.category);
+
+            // emits question to room
+            io.to(updatedRoom.room).emit('question',{
+                currentQuestion: currentQuestionThisRound,
+                questionCount: room.questionCount,
+                timerEnabled: room.timerEnabled,
+                timerDuration: room.timer,
+                question_id: questions[0].question_id,
+                question: questions[0].question,
+            })
+
+        }else{
+            // if max amount of question is reached ends game
+            console.log('Game over')
+
+            // gets score from all players in room and emits them to room
+            const usersInRoom = getUsersInRoom(room.room)
+            const userScores = usersInRoom.map(u => ({
+                name: u.name,
+                score: u.score
+            }))
+            const gameAnswersArray = room.playerAnswersArray;
+
+            io.to(room.room).emit('quizOver', userScores, gameAnswersArray);
         }
     }
 }
